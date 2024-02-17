976cad932a69143b0477c8f7ec9e4df5e3afec58
hoffmanp6358 <160301323+hoffmanp6358@users.noreply.github.com>
2024-02-16 23:50:48 -0500

    Add files via upload

---
+++ P1LAB1_HoffmanPhillip.py
@@ -0,0 +1,13 @@
+# Phillip Hoffman
+
+# 2/16/24
+
+# P1LAB1
+
+# I will create a program that that takes a first name input and creats a welcome message with that input.
+
+
+print('Enter your name:', end=' ')
+your_name = input()
+print('Hey', your_name)
+print('Welcome to zybooks!')
