75d507f4f111fae30e79d2b3185a41abc5cc5261
hoffmanp6358 <160301323+hoffmanp6358@users.noreply.github.com>
2024-02-22 00:32:31 -0500

    Add files via upload
    In this project I have created a program that will substitute an integer the user enters in some math problems.

---
+++ P1HW1_HoffmanPhillip.py
@@ -0,0 +1,21 @@
+# Phillip Hoffman
+
+ # 2/21/24
+
+ # P1HW1
+
+ # In this project I have created a program that will substitute an integer the user enters in some math problems.
+
+user_num = int(input('Enter integer:\n'))
+user_square = user_num ** 2
+user_cubed = user_num ** 3 
+
+print('You entered:' , user_num)
+print(user_num, 'squared is' , user_square)
+print('And' , user_num, 'cubed is' ,user_cubed , '!!')
+
+
+user_num2 = int(input('Enter another integer:\n'))
+
+print(user_num ,'+' , user_num2, 'is', user_num + user_num2)
+print(user_num ,'*' , user_num2, 'is', user_num * user_num2)
